0889e31ded9c294571898405a5db254f99ad3e0d
Changxinhh <nggtrang26@gmail.com>
2026-03-08 18:13:50 +0700

    IT203B

--- SS01/src/gioi4/Main.java
+++ SS01/src/gioi4/Main.java
@@ -1,7 +1,19 @@
 package gioi4;
 
+import java.io.IOException;
+
 public class Main {
+    public static void saveToFile() throws IOException {
+        throw new IOException();
+    }
+    public static void processUserData() throws IOException{
+        saveToFile();
+    }
     public static void main(String[] args) {
-        
+        try{
+            processUserData();
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
